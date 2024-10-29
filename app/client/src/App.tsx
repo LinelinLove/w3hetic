ec2cb71ca67321c53f84463aa4158de53520869d
Valentin Machefaux <valentin.machefaux@gmail.com>
2024-10-29 23:31:50 +0100

    -init Debut du projet

---
+++ app/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+        <div>c'est de la merde react</div>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ init/init.sql
@@ -0,0 +1,8 @@
+create database w3hetic_db;
+use `w3hetic_db`;
+
+create table `users` 
+(
+    
+    id int  not null auto_increment primary key,
+)
