5ff5355bd1743a5e3988074044e88a3f5436c952
LinelinLove <chrisline.lin@outlook.fr>
2024-10-31 23:41:46 +0100

    add icons header footer myfilespage fix css

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -3,19 +3,33 @@ import "./App.css";
 import Login from "./components/pages/LoginPage";
 import Signup from "./components/pages/SignUpPage";
 import Dashboard from "./components/pages/DashboardPage";
+import MyFiles from "./components/pages/MyFilesPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AuthProvider } from "./context/useAuth";
+import Footer from "./components/molecules/Footer";
+import Header from "./components/molecules/Header";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+        {/* <ProtectedRoute> */}
+        <Header />
+        {/* </ProtectedRoute> */}
         <Routes>
           <Route
             path="/"
             element={
               // <ProtectedRoute>
-                <Dashboard />
+              <Dashboard />
+              // </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/myfiles"
+            element={
+              // <ProtectedRoute>
+              <MyFiles />
               // </ProtectedRoute>
             }
           />
@@ -23,6 +37,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </AuthProvider>
   );
 }
--- app/client/src/components/atoms/Logo.tsx
+++ app/client/src/components/atoms/Logo.tsx
@@ -1,9 +1,13 @@
 const Logo = () => {
   return (
-  <div className='flex justify-center items-center w-full pt-6 lg:pt-12'>
-<img src="src/assets/w3hetic-logo.svg" alt="logo w3Hetic" className='w-64 lg:w-[300px]'/>
-</div>
-  )
-}
+    <div className="flex justify-center items-center w-full">
+      <img
+        src="src/assets/w3hetic-logo.svg"
+        alt="logo w3Hetic"
+        className="w-64 sm:w-[250px] drop-shadow-[0_5px_5px_rgba(0,0,0,0.9)]"
+      />
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
