f109025ea84ffa1c1f9bd9e31df702e7be194f85
Valentin Machefaux <valentin.machefaux@gmail.com>
2024-11-01 23:07:08 +0100

    ✨ Download file

--- app/server/controllers/file.controller.ts
+++ app/server/controllers/file.controller.ts
@@ -3,8 +3,6 @@ import { FileRepositoryI } from '../types/file.d'; // Assure-toi que le type est
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
-import crypto from 'crypto';
-import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
