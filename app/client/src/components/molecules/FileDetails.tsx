2b193e66bf90001d74955b4f2c6117e405305a47
LinelinLove <chrisline.lin@outlook.fr>
2024-11-02 03:56:56 +0100

    add icons link and download

--- app/client/src/components/molecules/FileDetails.tsx
+++ app/client/src/components/molecules/FileDetails.tsx
@@ -1,4 +1,9 @@
-import { faTrash, faFileZipper } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrash,
+  faFileZipper,
+  faLink,
+  faDownload,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface FileDetailsProps {
@@ -13,8 +18,16 @@ const FileDetails: React.FC<FileDetailsProps> = ({ fileName, uploadDate }) => {
         <FontAwesomeIcon icon={faFileZipper} className="pt-1" />
         <p className="break-all">{fileName}</p>
       </div>
-      <div className="flex flex-row items-center sm:gap-4 w-full sm:w-auto">
+      <div className="flex flex-row items-center gap-1 sm:gap-4 w-full sm:w-auto">
         <div className="flex-grow sm:flex-grow-0 text-center">{uploadDate}</div>
+        <FontAwesomeIcon
+          icon={faLink}
+          className="hover:text-blue-500 cursor-pointer transition ease-in-out delay-50"
+        />
+        <FontAwesomeIcon
+          icon={faDownload}
+          className="hover:text-emerald-600 cursor-pointer transition ease-in-out delay-50"
+        />
         <FontAwesomeIcon
           icon={faTrash}
           className="hover:text-red-600 cursor-pointer transition ease-in-out delay-50"
