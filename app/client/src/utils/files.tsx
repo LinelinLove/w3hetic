8793e643d26a5d423b8ec485c2ac64ca06a793a3
LinelinLove <chrisline.lin@outlook.fr>
2024-11-02 13:23:26 +0100

    link clipboard

--- app/client/src/utils/files.tsx
+++ app/client/src/utils/files.tsx
@@ -24,7 +24,7 @@ export const getFilenames = async (userId: number): Promise<any> => {
 
 export const deleteFile = async (fileId) => {
   const url = `${import.meta.env.VITE_API}/files/${fileId}`;
-  console.log("Sending DELETE request to:", url); // Log l'URL
+  console.log("Sending DELETE request to:", url);
   try {
     const response = await fetch(url, {
       method: "DELETE",
@@ -34,15 +34,40 @@ export const deleteFile = async (fileId) => {
     });
 
     if (!response.ok) {
-      const errorResponse = await response.json(); // Essaye de récupérer le message d'erreur
+      const errorResponse = await response.json();
       throw new Error(
         `Failed to delete: ${response.statusText} - ${errorResponse.message}`
       );
     }
 
-    return await response.json(); // Renvoie la réponse en JSON
+    return await response.json();
   } catch (error) {
     console.error("Error deleting file:", error);
+    throw error;
+  }
+};
+
+export const generateLink = async (fileId: number): Promise<any> => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API}/files/generate-link`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ file_id: fileId }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.link.download_link;
+  } catch (error) {
+    console.error("Error generating link:", error);
     throw error;
   }
 };
